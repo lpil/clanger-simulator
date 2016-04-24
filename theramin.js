eb2229fd23caea3304a0086e59cf9548cde7bc9d
Louis Pilfold <louis@lpil.uk>
2016-04-24 21:30:03 +0100

    Oscillator creation

--- theramin.js
+++ theramin.js
@@ -1,1 +1,5 @@
-console.log("Hello, world!");
+const context = new (window.AudioContext || window.webkitAudioContext)();
+const osc = context.createOscillator();
+osc.connect(context.destination);
+osc.frequency.value = 440;
+// osc.start();
