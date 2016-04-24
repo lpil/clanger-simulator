2b837c85f1bdef614dd63881090575b828823138
Louis Pilfold <louis@lpil.uk>
2016-04-24 22:25:06 +0100

    Frequency setting

--- theramin.js
+++ theramin.js
@@ -14,8 +14,17 @@ toggleGain.gain.value = 0;
 osc.frequency.value = 440;
 osc.start();
 
+function scaleFrequency(n) {
+  var min = Math.log(A0);
+  var max = Math.log(C8);
+  var scale = max - min;
+  return Math.exp(min + scale * n);
+}
+
 document.onmousemove = function(event) {
   const y = 1 - event.pageY / window.innerHeight;
+  const x = event.pageX / window.innerWidth;
+  osc.frequency.value  = scaleFrequency(x)
   mouseGain.gain.value = y;
 }
 
