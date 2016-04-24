69bec5b01aaf8921f9e865026059f173c4f08f6a
Louis Pilfold <louis@lpil.uk>
2016-04-24 21:30:28 +0100

    Calculate mouse position on mouse move

--- theramin.js
+++ theramin.js
@@ -3,3 +3,9 @@ const osc = context.createOscillator();
 osc.connect(context.destination);
 osc.frequency.value = 440;
 // osc.start();
+
+document.onmousemove = function(event) {
+  const x = event.pageX / window.innerWidth;
+  const y = event.pageY / window.innerHeight;
+  console.log(`x: ${x}, y: ${y}`);
+}
