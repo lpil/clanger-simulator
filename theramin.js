6423749fb667f9cc299b1d07a051fa02644b84d3
Louis Pilfold <louis@lpil.uk>
2016-04-24 22:02:29 +0100

    Gain control

--- theramin.js
+++ theramin.js
@@ -1,11 +1,23 @@
+const A0 = 27.5;
+const C8 = 4186;
+
 const context = new (window.AudioContext || window.webkitAudioContext)();
-const osc = context.createOscillator();
-osc.connect(context.destination);
+const osc     = context.createOscillator();
+const mouseGain  = context.createGain();
+const toggleGain = context.createGain();
+
+osc.connect(mouseGain);
+mouseGain.connect(toggleGain);
+toggleGain.connect(context.destination);
+
+toggleGain.gain.value = 0;
 osc.frequency.value = 440;
-// osc.start();
+osc.start();
 
 document.onmousemove = function(event) {
-  const x = event.pageX / window.innerWidth;
-  const y = event.pageY / window.innerHeight;
-  console.log(`x: ${x}, y: ${y}`);
+  const y = 1 - event.pageY / window.innerHeight;
+  mouseGain.gain.value = y;
 }
+
+document.onmousedown = function() { toggleGain.gain.value = 1; }
+document.onmouseup   = function() { toggleGain.gain.value = 0; }
